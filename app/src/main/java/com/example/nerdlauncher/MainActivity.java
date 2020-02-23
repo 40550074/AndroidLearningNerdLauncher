188bbc78b94452efd54b3931acaf6a5837d30778
40550074 <limengbeta@163.com>
2020-02-23 23:35:39 +1300

    show all activities

--- app/src/main/java/com/example/nerdlauncher/MainActivity.java
+++ app/src/main/java/com/example/nerdlauncher/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.nerdlauncher;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected Fragment createFragment() {
+        return NerdLauncherFragment.newInstance();
     }
 }
